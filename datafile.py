e62c18cb3b93f97aaf7fe8f68e28235287167a04
Aditya Kumar <61838094+ak8476@users.noreply.github.com>
2022-02-19 01:57:37 +0530

    Updated datafile.py

--- datafile.py
+++ datafile.py
@@ -4,7 +4,7 @@ import time
 
 place='India'
 
-response = urllib.request.urlopen("http://api.weatherapi.com/v1/current.json?key=f97bf394f3934328b4962723212010&q=%s&aqi=no"%(place))
+response = urllib.request.urlopen("http://api.weatherapi.com/v1/current.json?key=<YOURAPIKEY>&q=%s&aqi=no"%(place))
 data=json.loads(response.read())
 name=data['location']['name']
 times=time.ctime(data['location']['localtime_epoch'])
